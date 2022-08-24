6ed916a97fc0d3f865830bc59c8f1abeab6d9b65
ChiefChippy2 <chiefchippy2@gmail.com>
2022-08-24 22:46:46 +0200

    fix(incorrect prompt): -S requires a name

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Vérification du fichier .env en cours"
 python3.9 verify_env.py
 echo "Lancement du bot..."
 python3.9 src/main.py
-echo "Pour garder le bot en ligne, veuillez utiliser `screen -S bot python3.9 src/main.py`, ou bien `screen -S ./run.sh`"
+echo "Pour garder le bot en ligne, veuillez utiliser `screen -S bot python3.9 src/main.py`, ou bien `screen -S bot ./run.sh`"
