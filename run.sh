4a27f90b1fe95ba499bb0bf9ff616c41141c98f1
ChiefChippy2 <chiefchippy2@gmail.com>
2022-08-24 22:22:47 +0200

    fix(verify_env): Bad directory

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ echo "Installation de python3.9 en cours"
 echo "Installation des modules python en cours"
 ./install.sh
 echo "Vérification du fichier .env en cours"
-python3.9 src/verify_env.py
+python3.9 verify_env.py
 echo "Lancement du bot..."
 python3.9 src/main.py
-
-# Pour garder le bot en ligne, veuillez utiliser `screen python3.9 src/main.py`
+echo "Pour garder le bot en ligne, veuillez utiliser `screen -S bot python3.9 src/main.py`, ou bien `screen -S ./run.sh`"
