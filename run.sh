cf52bd636136d748ba315db7c96a27aa7174a7bc
ChiefChippy2 <chiefchippy2@gmail.com>
2022-09-09 21:58:04 +0200

    fix(interpolation)

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cd bot/
 echo "Vérification du fichier .env en cours"
 python3.9 verify_env.py
 echo "Lancement du bot..."
+echo "[ASTUCE] Pour garder le bot en ligne, veuillez utiliser \`screen -S bot python3.9 src/main.py\`, ou bien \`screen -S bot ./run.sh\`"
 python3.9 src/main.py
-echo "Pour garder le bot en ligne, veuillez utiliser `screen -S bot python3.9 src/main.py`, ou bien `screen -S bot ./run.sh`"
